c8fbc3f47802b51c89d2fca505d9127cd7741f79
hsiehtl <hsiehtl@princeton.edu>
2022-02-23 23:12:46 -0500

    update viz code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Daily and six hourly snapshots following a cluster 
+###Daily and six hourly snapshots following a cluster
+
+To run the example on binder: https://mybinder.org/v2/gh/tlhsieh/seed_movie/HEAD?labpath=example.ipynb
--- viz.py
+++ viz.py
@@ -1,45 +1,83 @@
 import numpy as np
-import imageio
 
-def crop(filename, outfilename='cropped.png'):
-    """Remove white borders around an image"""
+from bokeh.plotting import figure
+from bokeh.layouts import column
+from bokeh.models import CustomJS, ColumnDataSource, Slider
+from bokeh.models import LinearColorMapper, ColorBar
+from bokeh.io import output_notebook, show
+output_notebook()
 
-    image = imageio.imread(filename)
+import warnings
+warnings.filterwarnings("ignore", message="ColumnDataSource's columns must be of the same length")
 
-    # find indices of non-white pixels
-    shape = image.shape
-    gray_scale = np.mean(image, axis=2) # collapsing the RGB axis
-
-    ax0 = np.mean(gray_scale, axis=1) # collapsing axis 1, leaving only axis 0
-    idx_ax0_beg = np.argmax(ax0 < 255)
-    idx_ax0_end = shape[0] - np.argmax(np.flip(ax0) < 255)
-
-    ax1 = np.mean(gray_scale, axis=0) # collapsing axis 0, leaving only axis 1
-    idx_ax1_beg = np.argmax(ax1 < 255)
-    idx_ax1_end = shape[1] - np.argmax(np.flip(ax1) < 255)
-
-    cropped = image[idx_ax0_beg:idx_ax0_end, idx_ax1_beg:idx_ax1_end, :]
-    imageio.imsave(outfilename, cropped)
-
-def concat_img(filenames, outfilename='combined', axis=0):
-    """Combine a list of images to one. Input images can have different sizes. Output has the same extension as input.
-
-    Example:
-        concat_img(['input1.png', 'input2.png'], 'output', 1)
-
+def movie(da, y=None, x=None, xarray=False, sizefac=1, fontsize=12, vmin=None, vmax=None):
+    """Generates inline animation within Jupyter Notebook
+    
     Args:
-        axis: 0 for vertical stack; 1 for horizontal strip
+        da (np.array or xr.DataArray): A 3d array (e.g. t, y, x), where t is the axis being scanned through
+        y, x (1d array): y and x axes, not required if da is an xr.DataArray
+        xarray (bool): Specifies whether da is an xr.DataArray
+        sizefac (float): Factor to scale the size of output image
+    
     """
+    
+    # retrieve axes from xr.DataArray
+    if xarray:
+        d = da.values
+        y = da[da.dims[1]].values
+        x = da[da.dims[2]].values
+    else:
+        d = da
+        
+    if vmin == None:
+        vmin = np.min(d)
+    if vmax == None:
+        vmax = np.max(d)
 
-    images = [imageio.imread(filename) for filename in filenames]
-    maxax0 = np.max([image.shape[0] for image in images])
-    maxax1 = np.max([image.shape[1] for image in images])
+    # collect data to feed to JS
+    data = {'i': [d[0]], 'x': x, 'y': y}
+    for i in range(len(d)):
+        data[str(i)] = [d[i]]
 
-    padded = []
-    for image in images:
-        white = np.ones([maxax0, maxax1, image.shape[2]], dtype=image.dtype)*255
-        white[:image.shape[0], :image.shape[1], :] = image
-        padded.append(white)
+    source = ColumnDataSource(data=data)
+    source_ori = ColumnDataSource(data=data)
+    
+    color_mapper = LinearColorMapper(palette='Viridis256', low=vmin, high=vmax)
+    
+    plot = figure(x_range=(np.min(x), np.max(x)), y_range=(np.min(y), np.max(y)), plot_width=int(len(x)*sizefac), plot_height=int(len(y)*sizefac*0.8))
+    plot.image(image='i', x='x', y='y', dw=np.max(x)-np.min(x), dh=np.max(y)-np.min(y), source=source, color_mapper=color_mapper)
+    plot.add_layout(ColorBar(color_mapper=color_mapper, location=(0,0)), 'right')
+    
+    # change font size
+    fontsize_string = str(fontsize)+'pt'
+    plot.xaxis.major_label_text_font_size = fontsize_string
+    plot.xaxis.axis_label_text_font_size = fontsize_string
+    plot.yaxis.major_label_text_font_size = fontsize_string
+    plot.yaxis.axis_label_text_font_size = fontsize_string
 
-    combined = np.concatenate(padded, axis=axis)
-    imageio.imsave(outfilename+'.'+filenames[0].split('.')[-1], combined)
+    # set axis label
+    if xarray:
+        plot.xaxis.axis_label = da.dims[2]
+        plot.yaxis.axis_label = da.dims[1]
+        
+    slider = Slider(start=0, end=len(d)-1, value=0, step=1, title='index')
+
+    codes = """
+        var idx = slider.value;
+        var data = source.data;
+        var data_ori = source_ori.data;
+
+        data['x'] = []
+        data['y'] = []
+        data['i'] = []
+
+        data['x'] = data_ori['x'].slice();
+        data['y'] = data_ori['y'].slice();
+        data['i'] = data_ori[String(idx)].slice();
+
+        source.change.emit();
+    """
+    update_plot = CustomJS(args=dict(source=source, source_ori=source_ori, slider=slider), code=codes)
+    slider.js_on_change('value', update_plot)
+
+    show(column(slider, plot))
